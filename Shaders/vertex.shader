9a562dbc916888908261f55dfe80357c947015e0
Howard Huang <hahuang65@gmail.com>
2014-04-27 12:05:19 -0700

    Passing in original triangle coordinates via vertex attributes.

--- Shaders/vertex.shader
+++ Shaders/vertex.shader
@@ -1,7 +1,8 @@
 #version 410
 
-layout (location = 0) in vec4 offset;
+layout (location = 0) in vec4 position;
 layout (location = 1) in vec4 color;
+layout (location = 2) in vec4 offset;
 
 out VS_OUT
 {
@@ -10,11 +11,7 @@ out VS_OUT
 
 void main()
 {
-  const vec4 vertices[3] = vec4[3](vec4( 0.25, -0.25, 0.5, 1.0),
-                                   vec4(-0.25, -0.25, 0.5, 1.0),
-                                   vec4( 0.25, 0.25, 0.5, 1.0));
-
-  gl_Position = vertices[gl_VertexID] + offset;
+  gl_Position = position + offset;
 
   vs_out.color = color;
 }
--- game.cpp
+++ game.cpp
@@ -4,23 +4,23 @@ namespace Game {
   void setup(OpenGL::Program shaderProgram) {
     glClearColor( 0.f, 0.f, 0.f, 1.f );
 
-    float vertices[] = {
-         0.0f,  0.5f, // Vertex 1 (X, Y)
-         0.5f, -0.5f, // Vertex 2 (X, Y)
-        -0.5f, -0.5f  // Vertex 3 (X, Y)
+    static const float data[] = {
+      0.25, -0.25, 0.5, 1.0,
+      -0.25, -0.25, 0.5, 1.0,
+      0.25, 0.25, 0.5, 1.0
     };
 
     GLuint vbo;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
 
     GLuint vao;
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
 
     GLint posAttrib = glGetAttribLocation(shaderProgram.id(), "position");
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(posAttrib, 4, GL_FLOAT, GL_FALSE, 0, NULL);
     glEnableVertexAttribArray(posAttrib);
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -41,8 +41,9 @@ namespace Game {
                                 (float)sin(time) * 0.7f + 0.5f,
                                 0.0f, 1.0f };
 
-    glVertexAttrib4fv(0, trianglePosition);
     glVertexAttrib4fv(1, triangleColor);
+    glVertexAttrib4fv(2, trianglePosition);
+
     glDrawArrays(GL_PATCHES, 0, 3);
   }
 }
